36abcd3c3d66a35d6d61983c5d2216b423d415f8
TopStar <70pstar@gamil.com>
2024-03-07 14:17:59 +0200

    rollupOptions update

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,6 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,4 +9,21 @@ export default defineConfig({
     global: "globalThis",
     "process.env": {},
   },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@tests": path.resolve(__dirname, "./tests")
+    }
+  },
+  build: {
+    rollupOptions: {
+      external: [
+        "vue/server-renderer", 
+        "vue", 
+        "@safe-globalThis/safe-ethers-adapters", 
+        "@safe-globalThis/safe-core-sdk",
+        "@safe-globalThis/safe-ethers-lib"
+      ]
+    }
+  }
 });
